5957a4cc551d09b5a1bb8aff567d6e7f09300206
Noodle96 <jorgealfredomarino2@gmail.com>
2025-01-01 11:23:35 -0500

    Move player to the left and right

--- Assets/OurScripts/PlayerController.cs
+++ Assets/OurScripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     private CharacterController controller;
     private Vector3 direction;
     public float forwardSpeed = 10f;
+    private int desiredLine = 1; // 0:left 1: middle 2: right
+    public float laneDistance = 4f;
     void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -23,7 +25,37 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //read the inputs
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            desiredLine++;
+            if (desiredLine == 3) desiredLine = 2;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            desiredLine--;
+            if (desiredLine == -1) desiredLine = 0;
+        }
+        //Debug.LogWarning(Vector3.right); // (1,0,0)
+        //Debug.LogWarning(Vector3.left); // (-1,0,0)
+        //Debug.LogWarning(Vector3.zero); // (0,0,0)
+        //Debug.LogWarning(Vector3.up); // (0,1,0)
+        //Debug.LogWarning(Vector3.down); // (0,-1,0)
+        //Debug.LogWarning(Vector3.forward); // (0,0,1)
+        //Debug.LogWarning(Vector3.back); // (0,0,-1)
+
+        // Calcular donde deberia estar en el futuro
+        Vector3 zeta = transform.position.z * transform.forward; // escalar * vector3
+        Vector3 ye = transform.position.y * transform.up;
+        Vector3 targetPosition = zeta + ye;
+        //Debug.LogWarning("zeta: " +  zeta);
+        //Debug.LogWarning("ye: " + ye);
+        if (desiredLine == 0) {
+            targetPosition += Vector3.left * laneDistance;
+        } else if (desiredLine == 2) {
+            targetPosition += Vector3.right * laneDistance;
+        }
+        transform.position = targetPosition;
+        //transform.position = Vector3.Lerp(transform.position, targetPosition,80*Time.deltaTime);
     }
     private void FixedUpdate()
     {
